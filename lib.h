3332302b8951540d2db1c99398c4426f0dc00e78
Greginator9000 <tryptophan29@gmail.com>
2025-05-07 10:21:44 -0500

    Update lib.h
    lib header with set and get node

--- lib.h
+++ lib.h
@@ -1,1 +1,20 @@
+struct Node{
+Node* paths = Node*[4]; // [0] = north [1] = east [2] = south [3] = west
+int x;
+int y; 
+} 
 
+
+
+// 
+class lib{
+
+// lib functions 
+
+static void getNode(int x, int y); public 
+static void setNode(Node set); public
+
+
+
+
+}
