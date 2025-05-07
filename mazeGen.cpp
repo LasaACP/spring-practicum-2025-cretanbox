9dc06684d89f0cced9cab7fa39a495af64a76ce5
Collin-cpp <collinmccasland@icloud.com>
2025-05-07 10:51:34 -0500

    Update mazeGen.cpp

--- mazeGen.cpp
+++ mazeGen.cpp
@@ -2,14 +2,21 @@
 
 using namespace std; 
 
-Node mazeGen::Generate(Node s){
- Node c = s; 
- Node ohGodItsHideous[x][y]; 
+Node* mazeGen::Generate(Node* s){
+ Node* c = s; 
+ s->x = 0; 
+ s->y = 0; 
+ Node* ohGodItsHideous[x][y];
+ Node* thatsANewNode = new Node; 
+ ohGodItsHideous[0, y + 1] = thatsANewNode;
+ 
   while(false){
-    
-    else{
-      return s; 
+    if (c == s){
+     return s; 
     }
+   else{
+    if ()
+   }
   }
   return s; 
 }
