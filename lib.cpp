0ef5176a66db5e99bdf31b6069a488905b6b9d1e
Greginator9000 <tryptophan29@gmail.com>
2025-05-12 10:21:07 -0500

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,13 +5,44 @@ class lib{
 
 static Node getNode(int x, int y){
   
-}; 
+}
 static void setNode(Node set,int x,int y){
 
   getNode(x,y) = set;
   
-}; 
+} 
+static void display(Node s){
+//yoink from a solver and sort the x and y;
+//Node* grid = Node*[genx][geny]
 
+  bool full = false; // basically just "solve the maze" and put each node in the array.
+  while(!full){
+/*
+//while/for(something){} wow that loops stores in grid 2d array yippe 
+/
+full = true;
+//checker lmao 
+int y = 0;
+for(int x = 0; x < 10; x++){ 
+  if(grid[x][y] == nullptr)
+full = false;
+
+if(x == 9){
+x = 0;
+y++;
+}
+}
+
+
+
+
+
+
+*/
+    
+}
 
 }
+}
+
 
