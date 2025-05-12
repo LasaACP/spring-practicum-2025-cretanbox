b2fdf5f69f150ce16a94390dd1acb59595bd3216
Nolan-Knowhere <cadoiscool@gmail.com>
2025-05-12 10:55:58 -0500

    Update mazeGen.cpp

--- mazeGen.cpp
+++ mazeGen.cpp
@@ -4,20 +4,17 @@
 using namespace std; 
 
 Node* mazeGen::Generate(){
- //Node* c = s;
- //s->x = 0;
- //s->y = 0;
  x = 10;
  y = 10;
- Node* ohGodItsHideous[x][y];
+ Node* nodeMatrix[x][y];
  list<Node>* pushpop = new list<Node>*[(x*y)];
 
  for(int X = 0; X < x; X++){
   for(int Y = 0; Y < y; Y++) {
-   Node* thatsANewNode = new Node;
-   thatsANewNode->x = X;
-   thatsANewNode->y = Y;
-   ohGodItsHideous[X][Y] = thatsANewNode;
+   Node* newNode = new Node;
+   newNode->x = X;
+   newNode->y = Y;
+   nodeMatrix[X][Y] = newNode;
   }
  }
 
