16ac007974c0529dadfb671b0dcc8cca562cf821
Greginator9000 <tryptophan29@gmail.com>
2025-05-12 11:39:33 -0500

    Update main.cpp
    made a lil stuff into gen

--- main.cpp
+++ main.cpp
@@ -7,5 +7,12 @@
 
 int main() {
     std::cout<<"Bye World!"<<std::endl;
+    //Mazegen maze = new mazegen();
+
+
+    std::cout<<"how wide do you want your maze"<<std::endl;
+    //cin >> maze->x;
+    std::cout<<"how tall do you want your maze?"<<std::endl;
+    //cin >> maze->y;
     return 0;
 }
