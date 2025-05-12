d21b95e8b42d3a4147bcc842a330a637ecbfa214
Nolan-Knowhere <cadoiscool@gmail.com>
2025-05-12 10:01:49 -0500

    Update mazeGen.cpp

--- mazeGen.cpp
+++ mazeGen.cpp
@@ -7,7 +7,7 @@ Node* mazeGen::Generate(Node* s){
  Node* c = s; // what's this for? - CS
  s->x = 0; 
  s->y = 0; 
- Node* ohGodItsHideous[x][y];
+ Node* ohGodItsHideous[10][10];
 
  Node* thatsANewNode = new Node; 
  ohGodItsHideous[0][y + 1] = thatsANewNode;
