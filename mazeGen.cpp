70ce9b6b6a47197dd306de78fd6289170ff67154
Nolan-Knowhere <cadoiscool@gmail.com>
2025-05-12 10:28:33 -0500

    Update mazeGen.cpp

--- mazeGen.cpp
+++ mazeGen.cpp
@@ -3,25 +3,22 @@
 
 using namespace std; 
 
-Node* mazeGen::Generate(Node* s){
- Node* c = s; // what's c for? - CS
- s->x = 0; 
- s->y = 0; 
+Node* mazeGen::Generate(){
+ //Node* c = s;
+ //s->x = 0;
+ //s->y = 0;
  x = 10;
  y = 10;
  Node* ohGodItsHideous[x][y];
 
- Node* thatsANewNode = new Node; 
- ohGodItsHideous[0][0] = thatsANewNode;
- 
-
-  while(false){
-    if (c == s){
-     return s; 
-    }
-   else{
-    if (){}
-   }
+ for(int X = 0; X < x; X++){
+  for(int Y = 0; Y < y; Y++) {
+   Node* thatsANewNode = new Node;
+   thatsANewNode->x = X;
+   thatsANewNode->y = Y;
+   ohGodItsHideous[X][Y] = thatsANewNode;
   }
-  return s; 
+ }
+
+ while()
 }
