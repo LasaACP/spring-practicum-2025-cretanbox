5aeb084fac107dda2cad363b710e8e61cdc340f0
jeffreynorman <152411719+jeffreynorman@users.noreply.github.com>
2025-05-18 20:46:04 -0500

    main.cpp maze display?

--- main.cpp
+++ main.cpp
@@ -14,5 +14,43 @@ int main() {
     //cin >> maze->x;
     std::cout<<"how tall do you want your maze?"<<std::endl;
     //cin >> maze->y;
+    
+    mazeGen amazeing;
+    Node* theMaze = generator.Generate();
+    mazeDisplay(start);
+    
     return 0;
+}
+
+
+void mazeDisplay(Node* start) {
+    Node* maze[10][10] = {nullptr}; //assuming maze is 10x10 update later if this gets changed.
+    cout << " ";
+
+    //building the top wall first
+    for (int x = 0; x < 10; ++x) {
+        cout << (x == 0 ? "  " : " _");
+    }
+
+
+    for (int y = 0; y < 10; ++y) {
+        for (int x = 0; x < 10; ++x) {
+            Node* current = start;
+
+            
+            if (current->paths[3] == nullptr) {//left wall
+                cout << "|";
+            } else {
+                cout << " ";
+            }
+
+            
+            if (current->paths[2] == nullptr) { //bottom wall
+                cout << "_";
+            } else {
+                cout << " ";
+            }
+
+            
+            cout << "|" << endl; //right border
 }
